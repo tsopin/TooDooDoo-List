de92cda3a6766b3d08a96c9cc20d568223ab246c
Timofei Sopin <timofei.sopin@gmail.com>
2018-02-16 15:49:22 -0500

    Plist and Codable

--- TooDooDoo List/Controller/TodoViewController.swift
+++ TooDooDoo List/Controller/TodoViewController.swift
@@ -10,35 +10,43 @@ import UIKit
 
 class TodoViewController: UITableViewController {
     
-    var doArray = ["Task1", "Task2", "Task3"]
-    
-    let defaults = UserDefaults()
+    var doArray = [Item]()
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return doArray.count
     }
     
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
-        cell.textLabel?.text = doArray[indexPath.row]
+        
+        let item = doArray[indexPath.row]
+        
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+        
         
         return cell
     }
     
+    
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(doArray[indexPath.row])
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        doArray[indexPath.row].done = !doArray[indexPath.row].done
         
         tableView.deselectRow(at: indexPath, animated: true)
+        
+        saveItems()
     }
     
-   
+    
     
     @IBAction func addTaskButton(_ sender: UIBarButtonItem) {
         
@@ -46,11 +54,15 @@ class TodoViewController: UITableViewController {
         
         
         let alert = UIAlertController(title: "Add new Task", message: "", preferredStyle: .alert)
+        
         let action = UIAlertAction(title: "Add Task", style: .default) { (action) in
-           
-            self.doArray.append(textField.text!)
-            self.defaults.set(self.doArray, forKey: "ListArray")
-            self.tableView.reloadData()
+            
+            let newItem  = Item()
+            newItem.title = textField.text!
+            
+            self.doArray.append(newItem)
+            
+            self.saveItems()
         }
         
         
@@ -58,8 +70,6 @@ class TodoViewController: UITableViewController {
             alertTextField.placeholder = "Create new task"
             textField = alertTextField
         }
-        
-        
         
         alert.addAction(action)
         
@@ -70,11 +80,34 @@ class TodoViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let items = defaults.array(forKey: "ListArray") as? [String] {
-            doArray = items
+        
+        loadItems()
+    }
+    
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(doArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error encoding Item Array, \(error)")
         }
         
+        self.tableView.reloadData()
     }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+            doArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding Item Array, \(error)")
+            }
+    }
+    
+}
 }
 
 
---
+++ TooDooDoo List/Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  TooDooDoo List
+//
+//  Created by Timofei Sopin on 2018-02-16.
+//  Copyright © 2018 Centennial College. All rights reserved.
+//
+
+import UIKit
+
+class Item: Codable {
+    
+    var title : String = ""
+    var done : Bool = false
+   
+}
